a87812c8dd9b2a327f1d01ff51f09a3bf0096f37
Solgonadri <solgonadri@gmail.com>
2023-07-28 16:46:04 -0600

    Mensaje

--- src/components/formContacto/FormContacto.jsx
+++ src/components/formContacto/FormContacto.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import '../../components/pages/inicio/inicio.css'
+import { Input,Form, Col,Row } from 'antd'
 
-const FormContacto = () => {
+const FormContacto = () => { 
+  const { TextArea } = Input;
   return (
     <>
       <section id='form'>
@@ -27,6 +29,36 @@ const FormContacto = () => {
             <input type='text' className='typeData' id='type-asunto' />
           </div>
         </div>
+
+        <Form className="login-form">
+          <Row style={{backgroundColor: "white"}}>
+            <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8} className="hidden-sm hidden-xs bs-reset mt-login-5-bsfix">   
+                <div style={{"paddingTop" : "20px", width:"100%"}} className="inicio-sesion-proyecto-campos">
+                    <Form.Item name="nombre" rules={[
+                                  {max:50, message: "Número de caracteres permitidos: 50"},
+                                  {pattern: /^[^<>]*$/ , message:'Dato inválido, ingresa nuevamente.'}
+                                ]}>
+                        <Input
+                          //prefix={<UserOutlined style={{ color: "#d5007f" }}/>}
+                          id="inputNombre"
+                          placeholder="Nombre"
+                          name="nombre"
+                          maxLength={51}
+                        />
+                    </Form.Item>
+                </div>      
+            </Col>
+          </Row>
+          <Row style={{backgroundColor: "white"}}>
+            <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24} className="hidden-sm hidden-xs bs-reset mt-login-5-bsfix">   
+                <div style={{"paddingTop" : "20px", width:"100%"}} className="inicio-sesion-proyecto-campos">
+                    <Form.Item name="asunto">
+                        <TextArea autoSize={{ minRows: 12, maxRows: 20 }} className="obs-style" placeholder='Asunto'/>
+                    </Form.Item>
+                </div>      
+            </Col>
+          </Row>
+        </Form>
       </section>
     </>
   )
