f7360640add7ca5b346ab0e47969ac57de953b2b
Solgonadri <solgonadri@gmail.com>
2023-07-28 21:44:02 -0600

    Mensaje

--- src/components/formContacto/FormContacto.jsx
+++ src/components/formContacto/FormContacto.jsx
@@ -7,10 +7,16 @@ const FormContacto = () => {
   return (
     <>
       <section id='form'>
-        <h4 className='h-form'>¿Aún tienes dudas? Asesoría y Cotizaciones en 24 hrs.</h4>
         <Form className='login-form'>
           <Row>
-            <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} className='hidden-sm hidden-xs bs-reset mt-login-5-bsfix'>
+            <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24} style={{alignContent:"center"}}>
+                <div style={{ paddingTop: '20px', width: '100%' }} className='inpName'>
+                   <h4 className='h-form'>¿Aún tienes dudas? Asesoría y Cotizaciones en 24 hrs.</h4>
+                </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24} className='hidden-sm hidden-xs bs-reset mt-login-5-bsfix'>
               <div style={{ paddingTop: '20px', width: '100%' }} className='inpName'>
                 <Form.Item
                   name='name' rules={[
@@ -29,7 +35,7 @@ const FormContacto = () => {
             </Col>
           </Row>
           <Row>
-            <Col xs={6.0} sm={5.5} md={5.5} lg={5.3} xl={5.5} xxl={5} className='hidden-sm hidden-xs bs-reset mt-login-5-bsfix'>
+            <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} className='hidden-sm hidden-xs bs-reset mt-login-5-bsfix'>
               <div style={{ paddingTop: '20px', width: '100%' }} className='inpEmail'>
                 <Form.Item
                   name='email' rules={[
@@ -48,7 +54,7 @@ const FormContacto = () => {
             </Col>
           </Row>
           <Row>
-            <Col xs={6.0} sm={5.5} md={5.5} lg={5.3} xl={5.5} xxl={5} className='hidden-sm hidden-xs bs-reset mt-login-5-bsfix'>
+            <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} className='hidden-sm hidden-xs bs-reset mt-login-5-bsfix'>
               <div style={{ paddingTop: '20px', width: '100%' }} className='inpPhone'>
                 <Form.Item
                   name='phone' rules={[
@@ -67,7 +73,7 @@ const FormContacto = () => {
             </Col>
           </Row>
           <Row>
-            <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} className='hidden-sm hidden-xs bs-reset mt-login-5-bsfix'>
+            <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24} className='hidden-sm hidden-xs bs-reset mt-login-5-bsfix'>
               <div style={{ paddingTop: '20px', width: '100%' }} className='inpAsunto'>
                 <Form.Item
                   name='asunto' rules={[
@@ -86,7 +92,7 @@ const FormContacto = () => {
             </Col>
           </Row>
           <Row>
-            <Col xs={17} sm={17} md={17} lg={17} xl={9} xxl={17} className='hidden-sm hidden-xs bs-reset mt-login-5-bsfix'>
+            <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24} className='hidden-sm hidden-xs bs-reset mt-login-5-bsfix'>
               <div style={{ paddingTop: '20px', width: '100%' }} className='inpMessage'>
                 <Form.Item name='Message'>
                   <TextArea autoSize={{ minRows: 12, maxRows: 20 }} className='obs-style' placeholder='Escriba su Mensaje aqui...' />
