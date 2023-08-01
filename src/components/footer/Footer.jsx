a1f9fe7643d824e0ac99d857393e78896cf4c24e
Adriancit0-2112 <yael.solis21@gmail.com>
2023-07-31 22:10:29 -0600

    finaly

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,36 +1,34 @@
-import { Layout } from "antd";
-import "./Footer.scss";
-import logoINE from "../../assets/img/logo_hergo.jpg";
-import {FECHA_FINAL} from './../utils/constanst';
+import { Layout } from 'antd'
+import './Footer.scss'
+import logoINE from '../../assets/img/logo_hergo.jpg'
+import { FECHA_FINAL } from './../utils/constanst'
 
-
-export default function Footer() {
-  
-  const { Footer } = Layout;
+export default function Footer () {
+  const { Footer } = Layout
 
   return (
-    <Footer className="footer">
-      {/* Le puedes asignar un class directamente al componente de ant desig  sino sería ant-layout-footer*/}
-      <div className="footer__instituto">
-        <img className="footer__logo" src={logoINE} alt="Logo" />
-        {/*<div>
-           <span dangerouslySetInnerHTML={{ __html: "&copy;" }} /> 
+    <Footer className='footer'>
+      {/* Le puedes asignar un class directamente al componente de ant desig  sino sería ant-layout-footer */}
+      <div className='footer__instituto'>
+        <img className='footer__logo' src={logoINE} alt='Logo' />
+        {/* <div>
+           <span dangerouslySetInnerHTML={{ __html: "&copy;" }} />
           <span>© INE México {props.anio}</span>
           <span>
             <a href="https://cau.ine.mx/" target="blank">
               CAU
             </a>
           </span>
-        </div>*/}
+        </div> */}
       </div>
-      <div className="footer__area">
+      <div className='footer__area'>
         Protección de datos | Compatibilidad óptima con Google Chrome
       </div>
-      <div className="footer__version">
+      <div className='footer__version'>
         © Derechos Reservados
-        {/*<span>{VERSION_SISTEMA_TEXTO} {VERSION_SISTEMA}</span>*/}
+        {/* <span>{VERSION_SISTEMA_TEXTO} {VERSION_SISTEMA}</span> */}
         <span>Rev {FECHA_FINAL}</span>
       </div>
     </Footer>
-  );
+  )
 }
