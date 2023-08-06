5f1eed0d99e5f3bc70610df186fdb6f7401f9985
Adriancit0-2112 <yael.solis21@gmail.com>
2023-08-05 22:09:07 -0600

    Responsive

--- src/components/pages/inicio/Inicio.jsx
+++ src/components/pages/inicio/Inicio.jsx
@@ -15,44 +15,44 @@ import { Col, Row } from 'antd'
 const Inicio = () => {
   return (
 
-    <div className='div-inicio'>
-
-      <Row>
-        <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24} style={{ alignContent: 'center' }}>
-          <AwesomeSlider className='corrusel-react'>
-            <div data-src={c1} />
-            <div data-src={c2} />
-            <div data-src={c3} />
+    <div>
+      <Row style={{ alignItems: 'center', justifyContent: 'center', textAlign: 'center', position: 'relative', left: '24%' }}>
+        <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24} style={{ alignItems: 'center', justifyContent: 'center', textAlign: 'center' }}>
+          <AwesomeSlider className='corrusel-react' style={{ alignItems: 'center', justifyContent: 'center', textAlign: 'center' }}>
+            <div data-src={c1} style={{ alignItems: 'center', justifyContent: 'center', textAlign: 'center' }} />
+            <div data-src={c2} style={{ alignItems: 'center', justifyContent: 'center', textAlign: 'center' }} />
+            <div data-src={c3} style={{ alignItems: 'center', justifyContent: 'center', textAlign: 'center' }} />
           </AwesomeSlider>
         </Col>
       </Row>
 
-      <Row id='Row-center'>
-        <Col xs={4} sm={4} md={4} lg={4} xl={4} xxl={4} className='center-text' />
-        <Col xs={4} sm={4} md={4} lg={4} xl={4} xxl={4} className='center-text'>
+      <Row id='Row-center' style={{ alignItems: 'center', justifyContent: 'center', textAlign: 'center' }}>
+        <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} style={{ height: '0', width: '0' }}>
           <h1 className='Seccion-titulos'>¡Bienvenido a nuestro sitio web especializado
             en construcción, diseño e
             instalación de canceles y vidrios!
           </h1>
         </Col>
-        <Col xs={16} sm={16} md={16} lg={16} xl={16} xxl={16} className='center-text'>
-          <p id='p-1'>
-            En Hergo, somos apasionados por crear soluciones a la medida para embellecer y mejorar tus espacios. Ya sea que estés buscando darle un toque
-            elegante y moderno a tu hogar, o necesitas soluciones funcionales y seguras para tu negocio,
-            estamos aquí para ayudarte.
-          </p>
-        </Col>
+        <Row>
+          <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} style={{ alignItems: 'center', justifyContent: 'center', position: 'relative', left: '25%', textAlign: 'center' }}>
+            <p id='p-1'>
+              En Hergo, somos apasionados por crear soluciones a la medida para embellecer y mejorar tus espacios. Ya sea que estés buscando darle un toque
+              elegante y moderno a tu hogar, o necesitas soluciones funcionales y seguras para tu negocio,
+              estamos aquí para ayudarte.
+            </p>
+          </Col>
+        </Row>
       </Row>
 
-      <section id='section-2'>
-        <div className='text-2'>
+      <Row id='section-2' style={{ justifyContent: 'center', display: 'flex', flexWrap: 'wrap', alignContent: 'center', color: 'white' }}>
+        <Col xs={6} sm={6} md={6} lg={6} xl={6} xxl={6}>
           <h1 className='experiencia'>Nuestra experiencia</h1>
           <p className='p-experiencia'>Nuestra experiencia en la industria nos permite ofrecerte una amplia gama de servicios, desde el diseño personalizado de canceles y vidrios hasta su fabricación e instalación
             impecable. Trabajamos con materiales de la más alta calidad,
             asegurando resultados duraderos y satisfactorios.
           </p>
-        </div>
-        <div className='text-3'>
+        </Col>
+        <Col xs={6} sm={6} md={6} lg={6} xl={6} xxl={6}>
           <h2 className='destacar'>
             ¿Qué nos hace destacar?
           </h2>
@@ -61,36 +61,33 @@ const Inicio = () => {
             Estamos comprometidos con la excelencia en el
             servicio al cliente, brindando atención personalizada y asesoramiento experto en cada paso del proceso.
           </p>
-        </div>
-        <div className='unico-parrafo'>
-          <p className='parrafo-3'>
+        </Col>
+        <Col xs={6} sm={6} md={6} lg={6} xl={6} xxl={6}>
+          <p style={{ marginTop: '8%' }}>
             Explora nuestra galería de proyectos para inspirarte y descubrir cómo hemos transformado diversos
             espacios en impresionantes obras maestras. Desde elegantes mamparas de vidrio hasta
             modernos canceles para baños, cada proyecto es único y refleja el estilo y las necesidades de nuestros clientes.
           </p>
-        </div>
-        <div className='ideas'>
+        </Col>
+        <Col xs={6} sm={6} md={6} lg={6} xl={6} xxl={6}>
           <h3 className='ideas-h'>¿Tienes alguna idea en mente o necesitas asesoramiento para dar vida a tu proyecto?</h3>
           <p className='p-ideas'>Contáctanos y uno de nuestros expertos estará encantado de asesorarte y ofrecerte un presupuesto sin compromiso.</p>
-        </div>
-      </section>
-
-      <section id='section-3'>
-        <div className='text-4'>
+        </Col>
+      </Row>
+      <Row style={{ justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
+        <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} style={{ justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
           <h2 className='confianza'>
             ¡Confía en Hergo!
           </h2>
-          <p className='p-confianza'>Para convertir tus sueños en realidad y crear ambientes que te enamoren! Estamos
+          <p className='p-confianza' style={{ position: 'relative', left: '25%' }}>Para convertir tus sueños en realidad y crear ambientes que te enamoren! Estamos
             ansiosos por colaborar contigo en tu próximo proyecto de canceles y vidrios.
           </p>
-          <div className='bottom-Contactos'>
-            <Link to='hergoweb/contacto' className='link-3'>Contacto</Link>
-          </div>
-        </div>
-        <div className='img-section'>
+          <Link to='hergoweb/contacto' className='link-3'>Contacto</Link>
+        </Col>
+        <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} style={{ marginTop: '5%' }}>
           <img src={imgSecition} alt='' className='img-section4' />
-        </div>
-      </section>
+        </Col>
+      </Row>
       <FormContacto />
       <section id='photo'>
         <div id='txtHover'>
