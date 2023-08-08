d57acbc7bace64d10b827e21f0c6bab47a727f66
Adriancit0-2112 <yael.solis21@gmail.com>
2023-08-07 21:38:25 -0600

    Services

--- src/components/pages/servicios/Services.jsx
+++ src/components/pages/servicios/Services.jsx
@@ -1,6 +1,23 @@
+import { Row, Col } from 'antd'
+import S from '../../../assets/img/carrusel/Services.jpg'
+
 const Services = () => {
   return (
-    <h1>Services</h1>
+    <>
+      <Row style={{ textAlign: 'center', alignItems: 'center', justifyContent: 'center' }}>
+        <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} style={{ textAlign: 'center', alignItems: 'center', justifyContent: 'center' }}>
+          <img src={S} alt='' style={{ textAlign: 'center', alignItems: 'center', justifyContent: 'center', width: '100%' }} />
+          <Row>
+            <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24} style={{ marginTop: '5%' }}>
+              <h1 style={{ fontFamily: 'Gill Sans, Gill Sans MT, Calibri, Trebuchet MS, sans-serif', fontSize: '35px' }}>Nuestros Servicios</h1>
+              <p style={{ fontFamily: 'Gill Sans, Gill Sans MT, Calibri, Trebuchet MS, sans-serif', fontSize: '20px' }}>En Hergo, estamos comprometidos en brindarte soluciones de alta calidad y diseño personalizado en el campo de la construcción, diseño e instalación de canceles y vidrios. Nuestra amplia gama de servicios
+                está diseñada para transformar tus espacios en lugares elegantes, modernos y funcionales.
+              </p>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </>
   )
 }
 
