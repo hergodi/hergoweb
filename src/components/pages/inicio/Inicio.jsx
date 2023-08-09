15a2bbbc310e0be74e3200bd5f49388be3d0cf72
Adriancit0-2112 <yael.solis21@gmail.com>
2023-08-09 10:41:09 -0600

    Styles

--- src/components/pages/inicio/Inicio.jsx
+++ src/components/pages/inicio/Inicio.jsx
@@ -16,26 +16,26 @@ const Inicio = () => {
   return (
 
     <div>
-      <Row style={{ alignItems: 'center', justifyContent: 'center', textAlign: 'center', position: 'relative', left: '24%' }}>
-        <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24} style={{ alignItems: 'center', justifyContent: 'center', textAlign: 'center' }}>
-          <AwesomeSlider className='corrusel-react' style={{ alignItems: 'center', justifyContent: 'center', textAlign: 'center' }}>
-            <div data-src={c1} style={{ alignItems: 'center', justifyContent: 'center', textAlign: 'center' }} />
-            <div data-src={c2} style={{ alignItems: 'center', justifyContent: 'center', textAlign: 'center' }} />
-            <div data-src={c3} style={{ alignItems: 'center', justifyContent: 'center', textAlign: 'center' }} />
+      <Row>
+        <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24} style={{ alignItems: 'center', justifyContent: 'center', textAlign: 'center', display: 'flex' }}>
+          <AwesomeSlider className='corrusel-react' style={{ }}>
+            <div data-src={c1} />
+            <div data-src={c2} />
+            <div data-src={c3} />
           </AwesomeSlider>
         </Col>
       </Row>
 
       <Row id='Row-center' style={{ alignItems: 'center', justifyContent: 'center', textAlign: 'center' }}>
-        <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} style={{ height: '0', width: '0' }}>
-          <h1 className='Seccion-titulos'>¡Bienvenido a nuestro sitio web especializado
+        <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} style={{ height: '100%', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', textAlign: 'center' }}>
+          <h1 className='Seccion-titulos' style={{ fontSize: '30px', margin: '0', height: '100%', width: '100%' }}>¡Bienvenido a nuestro sitio web especializado
             en construcción, diseño e
             instalación de canceles y vidrios!
           </h1>
         </Col>
         <Row>
           <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} style={{ alignItems: 'center', justifyContent: 'center', position: 'relative', left: '25%', textAlign: 'center' }}>
-            <p id='p-1'>
+            <p id='p-1' style={{ marginTop: '7%', fontSize: '20px' }}>
               En Hergo, somos apasionados por crear soluciones a la medida para embellecer y mejorar tus espacios. Ya sea que estés buscando darle un toque
               elegante y moderno a tu hogar, o necesitas soluciones funcionales y seguras para tu negocio,
               estamos aquí para ayudarte.
@@ -46,17 +46,17 @@ const Inicio = () => {
 
       <Row id='section-2' style={{ justifyContent: 'center', display: 'flex', flexWrap: 'wrap', alignContent: 'center', color: 'white' }}>
         <Col xs={6} sm={6} md={6} lg={6} xl={6} xxl={6}>
-          <h1 className='experiencia'>Nuestra experiencia</h1>
-          <p className='p-experiencia'>Nuestra experiencia en la industria nos permite ofrecerte una amplia gama de servicios, desde el diseño personalizado de canceles y vidrios hasta su fabricación e instalación
+          <h1>Nuestra experiencia</h1>
+          <p>Nuestra experiencia en la industria nos permite ofrecerte una amplia gama de servicios, desde el diseño personalizado de canceles y vidrios hasta su fabricación e instalación
             impecable. Trabajamos con materiales de la más alta calidad,
             asegurando resultados duraderos y satisfactorios.
           </p>
         </Col>
         <Col xs={6} sm={6} md={6} lg={6} xl={6} xxl={6}>
-          <h2 className='destacar'>
+          <h2>
             ¿Qué nos hace destacar?
           </h2>
-          <p className='p-destacar'>
+          <p>
             Nuestro equipo altamente capacitado y dedicado, que se esfuerza por superar tus expectativas en cada proyecto.
             Estamos comprometidos con la excelencia en el
             servicio al cliente, brindando atención personalizada y asesoramiento experto en cada paso del proceso.
@@ -70,21 +70,21 @@ const Inicio = () => {
           </p>
         </Col>
         <Col xs={6} sm={6} md={6} lg={6} xl={6} xxl={6}>
-          <h3 className='ideas-h'>¿Tienes alguna idea en mente o necesitas asesoramiento para dar vida a tu proyecto?</h3>
-          <p className='p-ideas'>Contáctanos y uno de nuestros expertos estará encantado de asesorarte y ofrecerte un presupuesto sin compromiso.</p>
+          <h3>¿Tienes alguna idea en mente o necesitas asesoramiento para dar vida a tu proyecto?</h3>
+          <p>Contáctanos y uno de nuestros expertos estará encantado de asesorarte y ofrecerte un presupuesto sin compromiso.</p>
         </Col>
       </Row>
-      <Row style={{ justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
-        <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} style={{ justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
-          <h2 className='confianza'>
+      <Row>
+        <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} style={{ justifyContent: 'center', alignItems: 'center', textAlign: 'center', marginTop: '/0px' }}>
+          <h2>
             ¡Confía en Hergo!
           </h2>
-          <p className='p-confianza' style={{ position: 'relative', left: '25%' }}>Para convertir tus sueños en realidad y crear ambientes que te enamoren! Estamos
+          <p className='p-confianza' style={{ width: '100%', height: '100' }}>Para convertir tus sueños en realidad y crear ambientes que te enamoren! Estamos
             ansiosos por colaborar contigo en tu próximo proyecto de canceles y vidrios.
           </p>
           <Link href='contacto' to='hergoweb/contacto' className='link-3'>Contacto</Link>
         </Col>
-        <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} style={{ marginTop: '5%' }}>
+        <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} style={{ marginTop: '5%', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
           <img src={imgSecition} alt='' className='img-section4' />
         </Col>
       </Row>
