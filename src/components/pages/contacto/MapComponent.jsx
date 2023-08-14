eb184c032a35924983dc204bb642cbb8b1dfe3e4
Adriancit0-2112 <yael.solis21@gmail.com>
2023-08-14 11:42:19 -0600

    Mensaje

--- src/components/pages/contacto/MapComponent.jsx
+++ src/components/pages/contacto/MapComponent.jsx
@@ -16,13 +16,8 @@ function MapComponent () {
     })
   }, [])
   return (
-    <a
-      href={`https://www.google.com/maps/search/?api=1&query= ${19.39958953857422},${-98.93214416503906}`}
-      target='_blank'
-      rel='noopener noreferrer'
-    >
-      <div id='map' style={{ height: '400px', width: '100%' }} />
-    </a>
+    <div id='map' style={{ height: '400px', width: '100%' }} />
+
   )
 }
 
