7d96be79816843ba547965d7c1c583bfac5ebeb2
Solgonadri <solgonadri@gmail.com>
2023-08-16 18:35:36 -0600

    Nosotros

--- src/components/pages/contacto/MapComponent.jsx
+++ src/components/pages/contacto/MapComponent.jsx
@@ -3,8 +3,6 @@ import { GoogleMap, Marker } from '@react-google-maps/api'
 import { Wrapper } from '@googlemaps/react-wrapper';
 
 const MapComponent = () => {
-
-  const [map, setMap] = React.useState(null)
   
   const mapStyles = {
     height: "70vh",
@@ -18,31 +16,6 @@ const MapComponent = () => {
 
   const ubicacion = { lat: 19.39958953857422, lng: -98.93214416503906 }
 
-  const onLoad = React.useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(defaultCenter);
-    map.fitBounds(bounds);
-
-    setMap(map)
-  }, [])
-
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-  /*useEffect(() => {
-    const ubicacion = { lat: 19.39958953857422, lng: -98.93214416503906 }
-
-    const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: ubicacion,
-      zoom: 14
-    })
-
-    const Marker = new window.google.maps.Marker({
-      position: ubicacion,
-      map,
-      title: 'HERGO Diseño E Instalación'
-    })
-  }, [])*/
   return (
     <Wrapper apiKey="AIzaSyC1DYlcC8qPgkneJrBFcADwNQewzyFLyUs">
       <GoogleMap
