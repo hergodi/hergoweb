a6d0405595a6b04e25e6b4fcdc14b3dd3af2b89b
Solgonadri <solgonadri@gmail.com>
2023-08-22 11:16:24 -0600

    Nosotros

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "http://hergo.hergodi.com/"
+  base : "https://hergo.hergodi.com/"
 });
